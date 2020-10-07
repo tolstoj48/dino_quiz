569edd0ef71f66a40dbe9d0378ce42d4c909991a
Tolstoj48 <petr0musil@gmail.com>
2020-10-07 14:57:19 +0200

    Questions and answers UI added in appQuiz.js. HTML list of answers added.

--- js/appQuiz.js
+++ js/appQuiz.js
@@ -43,6 +43,19 @@ const ItemCtrl = (function () {
         yield questionsObj[property];
       }
     }
+
+  // Save answers to localStorage
+  const answers = {};
+  const saveAnswer = function saveAnswer(answer) {
+    if (localStorage.getItem("answers") === null || !localStorage.getItem("answers")) {
+      answers[0] = answer;
+      localStorage.setItem("answers", JSON.stringify(answers));
+    } else {
+      let localStorageAnswers = JSON.parse(localStorage.getItem("answers"));
+      localStorageAnswers[Object.keys(localStorageAnswers).length] = answer;
+      localStorage.setItem("answers", JSON.stringify(localStorageAnswers));
+    }
+    }
     
 
   return {
@@ -73,23 +86,26 @@ const ItemCtrl = (function () {
       let questions = this.getQuestions();
       console.log(questions);
       return getNextQuestion(questions);
+    },
+    saveAnswer: function (answer) {
+      saveAnswer(answer);
     }
   }
 })();
 
 // UI controler
 const UICtrl = (function () {
+  UIAnswersCounter = 1;  
   UISelectors = {
     quiz: ".quiz",
     recogn: ".recogn",
     gameType: "#game-type",
     resultTable: "#result-table",
-    questionImg: "#question-img",
-    question: "#question",
+    questionContainer: "#question-container",
+    answersList: "#answers-list",
     correctAnswers: "#correct-answers",
     incorrectAnswers: "#incorrect-answers",
     time: "#time",
-    press: "#press",
   }
   
   const eventListenersInit = function eventListenersInit() {
@@ -132,17 +148,26 @@ const UICtrl = (function () {
     })
 
 
+    // Click event on questions
     const clicker = ItemCtrl.getNextQuestion();
-    const buttonPress = document.querySelector(UISelectors.press);
-    buttonPress.addEventListener("click", function() {
-      console.log(clicker.next());    
-    })
+    // Skip first question - init is solved in initializeGameUIContent()
+    let endOfAnswerSet = clicker.next();
+    // Event listeners to answer list items    
+    const answerPress = document.querySelector(UISelectors.answersList);
+    answerPress.addEventListener("click", function() {
+      if (!endOfAnswerSet.done) {
+        nextQuestionUI(clicker);
+      } else {
+        console.log("This is the end!!!");
+      }
+    });
+
+    return clicker;
   }
 
   // Init of the game UI
-  const initilizeGameUIContent = function initilizeGameUI(questions) {
+  const initilizeGameUIContent = function initilizeGameUI(clicker) {
     let title; 
-
     // Set name of the game type
     let gameTypeName = document.querySelector(UISelectors.gameType);
     // Set title text of the game type
@@ -150,13 +175,79 @@ const UICtrl = (function () {
       title = "Kvíz - " + localStorage.getItem("gameId").toLowerCase();
     }
     gameTypeName.innerHTML = title;
+    // Question content after initialization
+    let questionContainer = document.querySelector(UISelectors.questionContainer);
+    let questionSet = ItemCtrl.getQuestions();
+    questionContainer.innerHTML = questionSet[0].question;
+    // Answers content after initialization
+    let html = ``;
+    let answersList = document.querySelector(UISelectors.answersList);
+    questionSet[0].answers.forEach((answer, index) => {
+      html += `<li id=${index} class="answer-item collection-item col s6">${answer}</li>`
+    })
+    answersList.innerHTML = html;
 
+    // Click event on answers
+    answersList.addEventListener("click", function (e) {
+      if (e.target.classList.contains("answer-item")) {
+        // Save answer to ItemCtrl
+        ItemCtrl.saveAnswer(e.target.innerHTML);
+        clicker.next();
+        // Change UI content based on clicker, ale s podmínkou, že pokud je true na generátoru - následně, pokud true, tak vyjodnocení
+      }
+    })
+  }
+
+  // Next question UI
+  const nextQuestionUI = function nextQuestionUI(clicker) {
+    let title; 
+    // Set name of the game type
+    let gameTypeName = document.querySelector(UISelectors.gameType);
+    // Set title text of the game type
+    if (localStorage.getItem("gameTypeId") === "quiz") {
+      title = "Kvíz - " + localStorage.getItem("gameId").toLowerCase();
+    }
+    gameTypeName.innerHTML = title;
+    // Question content - fetching next question
+    let questionSet = ItemCtrl.getQuestions();
+    // Last answer or not
+    if (UIAnswersCounter <= Object.keys(questionSet).length - 1) {
+      let questionContainer = document.querySelector(UISelectors.questionContainer);
+      console.log(UIAnswersCounter);
+      questionContainer.innerHTML = questionSet[UIAnswersCounter].question; // 
+      // Answers content after fetching next question
+      let html = ``;
+      let answersList = document.querySelector(UISelectors.answersList);
+      questionSet[UIAnswersCounter].answers.forEach((answer, index) => {
+        html += `<li id=${index} class="answer-item collection-item col s6">${answer}</li>`
+      })
+      answersList.innerHTML = html;
+
+      // Click event on answers
+      answersList.addEventListener("click", function (e) {
+        if (e.target.classList.contains("answer-item")) {
+          // Save answer to ItemCtrl
+          ItemCtrl.saveAnswer(e.target.innerHTML);
+          clicker.next();
+        }
+      })
+      // Counter of number of answered questions
+      UIAnswersCounter += 1;
+      
+    } else {
+      console.log("The end");
+      UIAnswersCounter = 1;
+      endOfGameStateUI(); /* dodělat totok - reset UIAnswersCOunter, smazat 
+      localStorage odpovědí, porovnat výstup z localsorage s objektem výsledků, 
+      zobrazit výslednou stránku a odkaz na výsledky předchozí, zobrazit čas*/
+  }
   }
 
   return {
     // Adding listeners
     eventListenersInit: function(){
-      eventListenersInit();
+      return eventListenersInit();
+
   },
     // Initialization of game
     initilizeGameUI: function (questions) {
@@ -165,6 +256,10 @@ const UICtrl = (function () {
       // Starts time in UI and ItemCtrl
       ItemCtrl.startGameTime();
     },
+    // Get answersCounter
+    getUIAnswersCounter: function() {
+      return UIAnswersCounter;
+    }
 
 }
 })();
@@ -177,11 +272,9 @@ const App = (function(UICtrl) {
   return {
     init: function () {
       // Loading event listeners
-      UICtrl.eventListenersInit();
-      // Get questions
-      const questions = ItemCtrl.getQuestions();
+      const clicker = UICtrl.eventListenersInit();
       // Inititalization of a game
-      UICtrl.initilizeGameUI(questions);
+      UICtrl.initilizeGameUI(clicker);
     },
   }
 
--- js/quizData.js
+++ js/quizData.js
@@ -1,33 +1,33 @@
 const quizArray = {
   all:{
-    1: {
+    0: {
       question: "Co máš k žrádlu?",
-      answers: ["Hovno", "skovno", "provno", "divno", "Hovno", "skovno", "provno", "divn"],
-      correctAnswer: 3,
+      answers: ["Hovno", "skovno", "provno", "divno"],
+      correctAnswer: "skovno",
+    },
+    1: {
+      question: "Co máš k spánku?",
+      answers: ["áčko", "béčk", "céčko", "déčko"],
+      correctAnswer: "provno",
     },
     2: {
-      question: "Co máš k spánku?",
-      answers: ["Hovno", "skovno", "provno", "divno", "Hovno", "skovno", "provno", "divn"],
-      correctAnswer: 3,
-    },
-    3: {
       question: "Co máš ke kuku?",
-      answers: ["Hovno", "skovno", "provno", "divno", "Hovno", "skovno", "provno", "divn"],
-      correctAnswer: 3,
+      answers: ["Hovno", "skovno", "provno", "divno"],
+      correctAnswer: "provno",
     },
   },
   herbivores:{
-    1: {
+    0: {
       question: "Dáš si trávu?",
-      answers: ["Hovno", "skovno", "provno", "divno", "Hovno", "skovno", "provno", "divn"],
-      correctAnswer: 3,
+      answers: ["Hovno", "skovno", "provno", "divno"],
+      correctAnswer: "divno",
     },
   },
   carnivores: {
-    1: {
+    0: {
       question: "Dáš si mašíšššššškoooo?",
-      answers: ["Hovno", "skovno", "provno", "divno", "Hovno", "skovno", "provno", "divn"],
-      correctAnswer: 3,
+      answers: ["Hovno", "skovno", "provno", "divno"],
+      correctAnswer: "provno",
     },
   }
   }
